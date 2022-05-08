09c586ccda82bd9fb5a302caf72d51c2ec33326e
Edi Kurniawan <edikurniawan.dev@gmail.com>
2022-05-08 17:26:10 +0700

    make migration table user

--- config/config.js
+++ config/config.js
@@ -1,26 +1,26 @@
 require('dotenv').config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
+const { DB_HOSTNAME, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 module.exports = {
     development: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_DATABASE,
-        host: DB_HOST,
+        host: DB_HOSTNAME,
         dialect: 'mysql',
     },
     test: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_DATABASE,
-        host: DB_HOST,
+        host: DB_HOSTNAME,
         dialect: 'mysql',
     },
     production: {
         username: DB_USERNAME,
         password: DB_PASSWORD,
         database: DB_DATABASE,
-        host: DB_HOST,
+        host: DB_HOSTNAME,
         dialect: 'mysql',
     },
 };
